d3490b9262af40e7797bfe8067217f41b704d5aa
bzm <bm.user.b@gmail.com>
2017-03-17 13:26:40 +0300

    Set on production

--- site_app/wsgi.py
+++ site_app/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # GETTING-STARTED: change 'myproject' to your project name:
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_app.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_app.settings.prod")
 
 application = get_wsgi_application()
