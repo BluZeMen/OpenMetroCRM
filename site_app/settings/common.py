90885426a6a8e102300e39f9c48577a3eb96dc8f
bzm <bm.user.b@gmail.com>
2017-03-17 10:20:53 +0300

    Added humanize

--- site_app/settings/common.py
+++ site_app/settings/common.py
@@ -87,6 +87,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.admin',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
