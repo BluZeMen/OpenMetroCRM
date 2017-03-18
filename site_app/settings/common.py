6ed5974ea4cde9c381f0c72e003ad68fd7aea57c
bzm <bm.user.b@gmail.com>
2017-03-18 15:10:54 +0300

    Changed config: smtp port

--- site_app/settings/common.py
+++ site_app/settings/common.py
@@ -67,7 +67,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-pulse.com'
 EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
 EMAIL_HOST_PASSWORD = SECRETS.get('mail_server_pass', '')
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 
 
